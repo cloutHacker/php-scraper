d24726eb7285757cde80216dfa71438b00b4ff59
kibiwottderick <kibiwottderick@gmail.com>
2022-08-06 01:47:11 -0700

    Awesome php scraper compressed and efficient code and example

--- src/index.php
+++ src/index.php
@@ -2,16 +2,8 @@
 include '../Resources/autoload.php';
 use Resources\Scraper;
 
-$source  = 'file:///D:/reactjs%20-%20How%20to%20show%20validation%20errors%20on%20form%20using%20React%20-%20Stack%20Overflow.html';
-$site = (new Scraper($source))->scrape()->filterTags('div')->exec();
-echo '<pre>';
-print_r($site);
-echo '</pre>';
-
-// $dom = new DOMDocument();
-// @$dom->loadHTML($site);
-// $h2s = $dom->getElementsByTagName('p');
-// foreach($h2s as $h2) {
-//     $text = $h2->textContent;
-//     echo $text.'<br>';
-// }
+$source  = 'https://www.jumia.co.ke/';
+$imgs = (new Scraper($source))->scrape()->filterTags('img')->filterAttr('data-src')->exec();
+foreach ($imgs as $img) {
+    echo '<img src='.$img.' height="300" width="400">';
+}
